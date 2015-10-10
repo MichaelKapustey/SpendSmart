072f1afd66181e09f25cd8b670fe52ca1a8a32a6
MichaelKapustey <michaelkapustey@gmail.com>
2015-10-10 23:08:35 +0300

    Models added

---
+++ SpendSmart/SpendSmart/SpendSmart/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpendSmart.Models
+{
+    public class Category
+    {
+    }
+}
---
+++ SpendSmart/SpendSmart/SpendSmart/Models/Spending.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SpendSmart.Models
+{
+    public class Spending
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+
+        public float Price { get; set; }
+
+        public Dictionary<string,object> Attacments { get; set; }
+    }
+}
